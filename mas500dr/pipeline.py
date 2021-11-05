7ccdec9aa0d8c10021d3dcc1d62e9e397ee56b15
Sara-Vitali <saravitali995@gmail.com>
2021-11-05 11:10:52 +0100

    message with changes

--- mas500dr/pipeline.py
+++ mas500dr/pipeline.py
@@ -52,8 +52,44 @@ class Pipeline:
         )
         self.master_bias.meta['combined'] = True
         self.master_bias.write(self.master_products_path / 'master_bias.fits')
+#    def create_master_dark(self):
+ #       pass
+
     def create_master_dark(self):
-        pass
+        dark_ccds = self.dark_ifc.ccds(ccd_kwargs=dict(unit='adu'))
+        print('Creating master dark...')
+        self.master_dark = cdp.combine(
+            dark_ccds,
+            method='average',
+            sigma_clip=True,
+            sigma_clip_low_thresh=5,
+            sigma_clip_high_thresh=5,
+            sigma_clip_func=np.ma.median,
+            sigma_clip_dev_func=mad_std,
+            mem_limit=350e6
+        )
+        self.master_dark.meta['combined'] = True
+        self.master_dark.write(self.master_products_path / 'master_dark.fits')
+
+
+"""
+This function is not similar as the one of the master_bias
+"""
+    def create_master_dark():
+        """
+        Following https://ccdproc.readthedocs.io/en/stable/reduction_toolbox.html#subtract-bias-and-dark
+        """
+        keys = ['file', 'imagetyp', 'object', 'filter', 'exposure']
+        ic1 = ImageFileCollection('path/to/your/calibration_folder', keywords=keys) # only keep track of keys
+
+        ic_dark = ImageFileCollection('calibration_dataset', glob_include='Dark*')
+        print('Creating master bias...')
+        matches_dark = (ic_dark.summary['imagetyp'] == 'Dark Frame') 
+        darks = ic_dark.summary['file'][matches_dark]
+        master_dark = CCDData(darks, unit=u.adu)
+
+        return master_dark
+
 
 
 def main():
@@ -61,6 +97,7 @@ def main():
     print(f'Current directory: {pwd}')
     pipe = Pipeline(Path(pwd))
     pipe.create_master_bias()
+    #pipe.create_master_dark()
     #print('This is script is under development.')
     #print('Right now it only prints this message.')
     #print('Soon it will serve to automatically reduce data from the MAS500 telescope.')
