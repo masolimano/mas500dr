4cd4801746cb6c9a6697e7c253f8c91d3076a278
Manuel Solimano <masolimano@uc.cl>
2021-11-10 18:24:31 -0300

    Refactoring the ccdproc.combine call and fixing the indentation problem with create_master_flat

--- mas500dr/pipeline.py
+++ mas500dr/pipeline.py
@@ -36,16 +36,24 @@ class Pipeline:
         self.flats_ifc = self.parent_ifc.filter(imagetyp='Flat Frame')
         self.light_ifc = self.parent_ifc.filter(imagetyp='Light Frame')
 
+    @staticmethod
+    def _avg_combine(frame_list):
+        """
+        Wrapper of ccdproc.combine with the keyword values recommended
+        in the ccdproc book for use with averaging.
 
-    def create_master_bias(self):
+        Parameters
+        ----------
+        frame_list: list of CCDData
+            List of frames to combine
+
+        Returns
+        ----------
+        combined_ccd: CCDData object
+
         """
-        Following https://www.astropy.org/ccd-reduction-and-photometry-guide/v/dev/notebooks/02-04-Combine-bias-images-to-make-master.html
-        section 2.3.3.1
-        """
-        bias_ccds = self.bias_ifc.ccds(ccd_kwargs=dict(unit='adu'))
-        print('Creating master bias...')
-        self.master_bias = cdp.combine(
-            bias_ccds,
+        combined_ccd = cdp.combine(
+            frame_list,
             method='average',
             sigma_clip=True,
             sigma_clip_low_thresh=5,
@@ -54,7 +62,34 @@ class Pipeline:
             sigma_clip_dev_func=mad_std,
             mem_limit=350e6
         )
-        self.master_bias.meta['combined'] = True
+        combined_ccd.meta['combined'] = True
+        return combined_ccd
+
+    def create_master_dark(self):
+        """
+        Combines all available dark frames after master bias subtraction.
+        TODO: - combine only darks with equal exposure length
+              - identify hot pixels
+        """
+        dark_ccds = self.darks_ifc.ccds(ccd_kwargs=dict(unit='adu'))
+        print('Creating master dark...')
+        bias_subtracted_darks = list()
+        for dark in dark_ccds:
+            bias_subtracted_dark = cdp.subtract_bias(dark, self.master_bias)
+            bias_subtracted_darks.append(bias_subtracted_dark)
+
+        self.master_dark = self._avg_combine(bias_subtracted_darks)
+        self.master_dark.write(self.master_products_path / 'master_dark.fits', overwrite=True)
+
+
+    def create_master_bias(self):
+        """
+        Following https://www.astropy.org/ccd-reduction-and-photometry-guide/v/dev/notebooks/02-04-Combine-bias-images-to-make-master.html
+        section 2.3.3.1
+        """
+        bias_ccds = self.bias_ifc.ccds(ccd_kwargs=dict(unit='adu'))
+        print('Creating master bias...')
+        self.master_bias = self._avg_combine(bias_ccds)
         self.master_bias.write(self.master_products_path / 'master_bias.fits', overwrite=True)
 
     def create_master_dark(self):
@@ -84,32 +119,20 @@ class Pipeline:
         self.master_dark.write(self.master_products_path / 'master_dark.fits', overwrite=True)
 
 
+
+
+    def create_master_flat(self):
         """
         Flat correction: bias substracion, assuming dark current = 0
         """
-    
-    
-        def create_master_flat(self):
-            flat_ccds = self.flats_ifc.ccds(ccd_kwargs=dict(unit='adu'))
-            print('Creating master flat...')
-            bias_subtracted_flats = list()   
-            for flat in flat_ccds:
-                bias_subtracted_flat = cdp.subtract_bias(flat, self.master_bias)
-                bias_subtracted_flats.append(bias_subtracted_flat)
-                
-            self.master_flat = cdp.combine(
-             bias_subtracted_flats,
-             method='average',
-             sigma_clip=True,
-             sigma_clip_low_thresh=5,
-             sigma_clip_high_thresh=5,
-             sigma_clip_func=np.ma.median,
-             sigma_clip_dev_func=mad_std,
-             mem_limit=350e6
-            )
-            self.master_flat.meta['combined'] = True
-            self.master_flat.write(self.master_products_path / 'master_flat.fits', overwrite=True)
-    
+        flat_ccds = self.flats_ifc.ccds(ccd_kwargs=dict(unit='adu'))
+        print('Creating master flat...')
+        bias_subtracted_flats = list()
+        for flat in flat_ccds:
+            bias_subtracted_flat = cdp.subtract_bias(flat, self.master_bias)
+            bias_subtracted_flats.append(bias_subtracted_flat)
+        self.master_flat = self._avg_combine(bias_subtracted_flats)
+        self.master_flat.write(self.master_products_path / 'master_flat.fits', overwrite=True)
 
 def main():
     pwd = os.getcwd()
